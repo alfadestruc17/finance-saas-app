37f6ede57460f7b9bdc8e6ada344f0fc960dbc80
Samuel <rene28609@gmail.com>
2025-08-07 15:08:49 -0500

    installación de unos paquetes e importaciones

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge';
 
@@ -13,22 +14,6 @@ app.get('/hello', (c) => {
         message: "hello world"
     })
 })
-    .get('/hello/:test',
-        (c) => {
-            return c.json({
-                message: "hello world"
-            })
-        })
-    .post("/",
-        zValidator("json", z.object({
-            name: z.string(),
-            userId: z.string(),
-        })),
-        (c) => {
-            const { } = c.req.valid("json");
-
-            return c.json({})
-        })
 
 export const POST = handle(app);
 export const GET = handle(app);
