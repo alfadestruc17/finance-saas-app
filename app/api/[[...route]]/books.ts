cad9f9c74357dc1b067f9da0862eab989ab003eb
Samuel <rene28609@gmail.com>
2025-08-07 15:22:32 -0500

    creacion de archivo books y cambios en la rutas

--- app/api/[[...route]]/books.ts
+++ app/api/[[...route]]/books.ts
@@ -1,10 +1,10 @@
-// authors.ts
+// books.ts
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.json('list authors'))
-app.post('/', (c) => c.json('create an author', 201))
+app.get('/', (c) => c.json('list books'))
+app.post('/', (c) => c.json('create a book', 201))
 app.get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
 
 export default app
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,17 @@
-import { z } from 'zod';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { zValidator } from '@hono/zod-validator';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
 
-app
-    .get('/hello',
-        clerkMiddleware(),
-        (c) => {
-            const auth = getAuth(c);
-
-            if (!auth?.userId) {
-                return c.json({ error: 'Unauthorized' });
-            }
-        return c.json({
-            message: "hello world",
-            userId: auth.userId,
-        })
-    })
+app.route("/authors", authors);
+app.route("/books", books);
+    
 
 export const POST = handle(app);
 export const GET = handle(app);
