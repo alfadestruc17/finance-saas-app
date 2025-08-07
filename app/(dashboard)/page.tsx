4b74e66068da2b1142916cd7ee4197ffca13fbd2
Samuel <rene28609@gmail.com>
2025-08-07 12:02:37 -0500

    autentificacion completada

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
+
+
 
 export default function Home() {
   return (
-    <p>
-      this is a autenticated route
-    </p>
+    <UserButton afterSignOutUrl="/"/>
   )
 }
