0c7199e9cafd8dad6ecd5186e88412167c559a4b
Samuel <rene28609@gmail.com>
2025-08-08 12:35:23 -0500

    pruebas de la correcta conexion para obtener los datos

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,10 +1,19 @@
-import { accounts } from '@/db/schema'
 import { Hono } from 'hono'
 
+import { db } from '@/db/drizzle'
+import { accounts } from '@/db/schema'
 
 const app = new Hono()
-    .get("/", (c) => {
-        return c.json({ accounts: [] })
+    .get("/", async (c) => {
+        const data = await db
+            .select({
+                id: accounts.id,
+                name: accounts.name,
+
+            })
+            .from(accounts)
+
+        return c.json({ data })
     })
 
 export default app 
