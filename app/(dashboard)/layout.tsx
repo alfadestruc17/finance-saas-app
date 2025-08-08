de7d9430ee399e5b577198ab62ca4fb46c4a31a0
Samuel <rene28609@gmail.com>
2025-08-07 23:40:59 -0500

    primera parte del header con logo

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,18 @@
+import { Header } from "@/components/Header";
+
 type Props = {
     children: React.ReactNode;
 }
 
-const DashboardLayout = ({children} : Props) => {
-    return ( 
-        <div>
-            {children}
-        </div>
-     );
+const DashboardLayout = ({ children }: Props) => {
+    return (
+        <>
+            <Header />
+            <main className="px-3 lg:px-14">
+                {children}
+            </main>
+        </>
+    );
 }
 
 
