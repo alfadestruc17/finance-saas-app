d8cd33e0f73f488c9d7f606f9f6f30802edb4e36
Samuel <rene28609@gmail.com>
2025-08-08 23:49:03 -0500

    creacion de hooks para reutilizacion de codigo

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { UserButton } from "@clerk/nextjs"
 
 
 
 export default function Home() {
+
+  const { onOpen } = useNewAccount();
+
   return (
     <div>
-      Dashboard page
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
     </div>
   )
 }
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,4 @@
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import {
 
     Sheet,
@@ -10,8 +11,12 @@ import {
 
 
 export const NewAccountSheet = () => {
+
+    const {isOpen, onClose } = useNewAccount();
+
+
     return (
-        <Sheet open>
+        <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
                 <SheetHeader>
                     <SheetTitle>
