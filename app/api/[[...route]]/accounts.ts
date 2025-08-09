c958f1615d9518f822d5acda0988b3a1a1a3da44
Samuel <rene28609@gmail.com>
2025-08-08 21:06:21 -0500

    mejora en el manejo de excepciones con HTTPException

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,18 +2,35 @@ import { Hono } from 'hono'
 
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { HTTPException } from 'hono/http-exception'
+
 
 const app = new Hono()
-    .get("/", async (c) => {
-        const data = await db
-            .select({
-                id: accounts.id,
-                name: accounts.name,
+    .get(
+        "/",
+        clerkMiddleware(),
 
-            })
-            .from(accounts)
+        async (c) => {
+            const auth = getAuth(c);
 
-        return c.json({ data })
-    })
+            if (!auth?.userId) {
+                throw new HTTPException(401, {
+                    res: c.json({ error: "Unauthorized" }, 401)
+                })
+            }
+
+
+
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+
+                })
+                .from(accounts)
+
+            return c.json({ data })
+        })
 
 export default app 
