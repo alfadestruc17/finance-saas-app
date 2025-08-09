d9b1f6c11a4371a5e3d828d3ff3b78c292fcdf1a
Samuel <rene28609@gmail.com>
2025-08-09 09:20:37 -0500

    primera vista de accounts

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,7 +1,34 @@
+"use client"
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Plus } from "lucide-react";
+
+
 const AccountsPage = () => {
+
+    const newAccount = useNewAccount();
+
+
     return (
         <div>
-            Accounts page
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts page
+                    </CardTitle>
+                    <Button onClick={newAccount.onOpen} size="sm">
+                        <Plus className="size-4 mr-2" />
+                        Add new
+                    </Button>
+                </CardHeader>
+            </Card>
         </div>
     )
 };
