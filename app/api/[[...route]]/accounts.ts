8a7155446ca300b00baa250dfba393f314d8ac68
Samuel <rene28609@gmail.com>
2025-08-08 22:49:43 -0500

    primeras impresiones del post

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
 
 import { db } from '@/db/drizzle'
-import { accounts } from '@/db/schema'
+import { accounts, insertAccountSchema } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { HTTPException } from 'hono/http-exception'
-
+import { zValidator } from '@hono/zod-validator'
 
 const app = new Hono()
     .get(
@@ -31,6 +31,23 @@ const app = new Hono()
                 .from(accounts)
 
             return c.json({ data })
+        })
+    .post(
+        "/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json")
+
+            if (!auth?.userId) {
+                throw new HTTPException(401, {
+                    res: c.json({ error: "Unauthorized" }, 401)
+                })
+            }
+            return c.json({})
         })
 
 export default app 
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
-
+import { createInsertSchema } from "drizzle-zod";
 
 
 export const accounts = pgTable("accounts", {
@@ -8,3 +8,5 @@ export const accounts = pgTable("accounts", {
     name: text("name").notNull(),
     userId: text("user_id").notNull(),
 })
+
+export const insertAccountSchema = createInsertSchema(accounts);
