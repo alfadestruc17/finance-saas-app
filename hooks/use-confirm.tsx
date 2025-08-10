445c90d30e4693a5e586acdb43cddce131eabdf6
Samuel <rene28609@gmail.com>
2025-08-10 10:15:19 -0500

    mensaje de confirmacion  para eliminar y cancelar accion

---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,65 @@
+import { JSX, useState } from "react";
+
+
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog"
+import { resolve } from "path";
+
+
+
+export const useConfirm = (
+    title: string,
+    message: string,
+): [() => JSX.Element, () => Promise<unknown>] => {
+    const [promise, setPromise] = useState<{ resolve: (value: boolean) => void } | null>(null);
+    const confirm = () => new Promise((resolve, reject) => {
+        setPromise({ resolve });
+    });
+
+    const handleClose = () => {
+        setPromise(null);
+    };
+
+    const handleConfirm = () => {
+        promise?.resolve(true);
+        handleClose();
+    };
+
+    const handleCancel = () => {
+        promise?.resolve(false);
+        handleClose();
+    }
+
+    const confirmationDialog = () => (
+        <Dialog open={promise !== null}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{message}</DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="pt-2">
+                    <Button 
+                        onClick={handleCancel}
+                        variant="outline"
+                    >
+                        Cancelar
+                    </Button>
+                    <Button 
+                        onClick={handleConfirm}
+                    >
+                        Confirmar
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+    return [confirmationDialog, confirm]
+
+};
