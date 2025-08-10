f16bd5d04d409b9e73a6683098ae8fe9677489d6
Samuel <rene28609@gmail.com>
2025-08-09 21:42:29 -0500

    animacion de carga o loading

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
@@ -15,6 +15,7 @@ import {
 
 import { columns } from "./colums";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 const AccountsPage = () => {
@@ -22,6 +23,24 @@ const AccountsPage = () => {
     const newAccount = useNewAccount();
     const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
+
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-34">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader >
+                        <Skeleton className="h-8 w-48" />     
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-34">
@@ -40,7 +59,7 @@ const AccountsPage = () => {
                         filterKey="email"
                         columns={columns}
                         data={accounts}
-                        onDelete={() => {}}
+                        onDelete={() => { }}
                         disabled={false}
                     />
                 </CardContent>
