993d6ce91afa8e799f42a1c0ffe9d772fbeb60d1
Samuel <rene28609@gmail.com>
2025-08-13 11:32:37 -0500

    use get individual completado

--- features/transactions/api/use-get-transaction.ts
+++ features/transactions/api/use-get-transaction.ts
@@ -2,15 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
 
-export const useGetAccount = (id?: string) => {
+export const useGetTransaction = (id?: string) => {
     const query = useQuery({
         enabled: !!id,
-        queryKey: ["account", { id }],
+        queryKey: ["transaction", { id }],
         queryFn: async () => {
-            const response = await client.api.accounts[":id"].$get({ param: { id } });
+            const response = await client.api.transactions[":id"].$get({ param: { id } });
 
             if (!response.ok) {
-                throw new Error("Error fetching account");
+                throw new Error("Error fetching transactions");
             }
             const { data } = await response.json();
             return data;
