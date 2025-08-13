5787cd3ece0057c9025e11aaa50eef50814c0810
Samuel <rene28609@gmail.com>
2025-08-13 00:07:50 -0500

    creacion de las rutas y arreglos en los get post patch delete

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,8 +3,10 @@ import { handle } from 'hono/vercel'
 
 import accounts from './accounts'
 import categories from './categories';
+import transactions from "./transactions"
 
 import { HTTPException } from 'hono/http-exception';
+
 
 export const runtime = 'edge';
 
@@ -20,7 +22,8 @@ app.onError((err, c) => {
 
 const routes = app
     .route("/accounts", accounts)
-    .route("/categories", categories);
+    .route("/categories", categories)
+    .route("/transactions", transactions)
 
 export const POST = handle(app);
 export const GET = handle(app);
